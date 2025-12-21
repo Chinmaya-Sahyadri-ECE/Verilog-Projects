12358a5050111e086403be8f260e3bd60fba011d
Chinmaya-Sahyadri-ECE <chinmayaganapathi@gmail.com>
2025-12-21 23:07:29 +0530

    Update alu.v

--- 32 Bit ALU/Design Block/alu.v
+++ 32 Bit ALU/Design Block/alu.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.11.2024 20:46:38
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu(En,opcode,A,B,result);
 input En;
